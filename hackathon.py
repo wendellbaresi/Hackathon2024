b947c219bc20759269350b9faaa1ee975e5e288d
Wendell Baresi <101334168+wendellbaresi@users.noreply.github.com>
2024-09-11 17:40:16 -0300

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -4,6 +4,10 @@ speaker = pyttsx3.init() #Iniciando a biblioteca que foi baixada#
 
 texto = "Ola Mundo, estamos participando do HACKATHON 2024, e somos o grupo 4" #Criando a variavel de texto, para fazer a leitura#
 
+Ler = open('Local do arquivo.txt', 'r' , encoding = 'utf8') #Variavel para abrir um arquivo e fazer a leitura, seguindo as normas e caracteres portugues UTF-8
+
+texto = Ler.read() # função para fazer a leitura
+
 voices = speaker.getProperty('voices') #Criando a variavel que vai ser definida como propriedade, que vai salvar as vozes do computador#
 
 for voice in voices: #Definição de vozes do computador, ao executar vai mostrar quais vozes o seu computador tem. Exemplo: Voz em Inglês, Voz em Português ou Voz em Espanhol#
